3e8966fa61636c268f2519b0fc2bdbdcfcf5259c
Claire Tsai <claire.tsai@onelab.tw>
2021-11-11 00:32:30 +0800

    feat: add bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
   </div>
 </template>
 
